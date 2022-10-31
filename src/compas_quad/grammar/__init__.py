17c49420a421f1eb981304dfa48006d31200bfa2
Rafael Pastrana <arpj@princeton.edu>
2022-10-31 14:44:20 -0400

    [Grammar] Split `string_*` methods from `lizard.py` to `string.py`

--- src/compas_quad/grammar/__init__.py
+++ src/compas_quad/grammar/__init__.py
@@ -1,5 +1,11 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 from .addition import *  # noqa F403
 from .deletion import *  # noqa F403
 from .lizard import *  # noqa F403
+from .string import *  # noqa F403
+
 
 __all__ = [name for name in dir() if not name.startswith('_')]
--- src/compas_quad/grammar/lizard.py
+++ src/compas_quad/grammar/lizard.py
@@ -1,12 +1,5 @@
-from __future__ import print_function
-from __future__ import absolute_import
-from __future__ import division
-
 from .addition import add_strip
 from .deletion import delete_strip
-
-from itertools import product
-from random import random
 
 
 __all__ = ['Lizard']
@@ -19,7 +12,7 @@ class Lizard:
         self.grow = False
         self.mesh = quad_mesh
 
-    def initiate(self, tail=None, head=None):
+    def init(self, tail=None, head=None):
         if tail in self.mesh.vertex and head in self.mesh.vertex and tail in self.mesh.vertex[head]:
             self.lizard = [head, tail]
         else:
@@ -54,8 +47,7 @@ class Lizard:
     def delete(self):
         if self.grow:
             # use update polyedge
-            print('Not Implemented')
-            pass
+            raise NotImplementedError
 
         skey = self.mesh.edge_strip((self.lizard[0], self.lizard[1]))
         self.pivot()  # check that pivot ends along strip
@@ -100,72 +92,3 @@ class Lizard:
                 self.add()
             elif k == 'd':
                 self.delete()
-
-
-def string_generation_brute(characters, length):
-    for letters in product(characters, repeat=length):
-        yield ''.join(letters)
-
-
-def string_generation_random(characters, number, length, ratios=None):
-    if not ratios:
-        ratios = [1.0 / len(characters)] * len(characters)
-
-    for _ in range(number):
-        string = ''
-        for _ in range(length):
-            x = random()
-            for i in range(len(characters)):
-                if x < sum(ratios[:i + 1]):
-                    string += characters[i]
-                    break
-        yield string
-
-
-def string_generation_structured(characters, number, length):
-
-    for _ in range(number):
-        string = ''
-        polyedge_length = 0  # number of 't' between odd and even pari of 'a'
-
-        for i in range(length):
-
-            # add strip before end of string if polyedge is being collected
-            if i == length - 1 and polyedge_length != 0:
-                string += 'a'
-                polyedge_length = 0
-                continue
-
-            x = random()
-
-            # NO polyedge being collected - uniform chances
-            if polyedge_length == 0:
-                if x < 0.33:
-                    string += 't'
-                elif x < 0.67:
-                    string += 'p'
-                else:
-                    string += 'a'
-                    polyedge_length += 1
-
-            # polyedge being collected - more chances to obtain 't' or 'p'
-
-            # do not add if polyedge has only one vertex
-            elif polyedge_length == 1:
-                if x < 0.5:
-                    string += 't'
-                    polyedge_length += 1
-                else:
-                    string += 'p'
-
-            else:
-                if x < 0.4:
-                    string += 't'
-                    polyedge_length += 1
-                elif x < 0.8:
-                    string += 'p'
-                else:
-                    string += 'a'
-                    polyedge_length = 0
-
-        yield string
